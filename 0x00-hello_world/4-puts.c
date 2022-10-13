b3d52c35b89059e13cbd10b1a994b5047c2e268b
mg-cmd <gmebrahtu8@gmail.com>
2022-10-13 12:45:08 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,41 +1,19 @@
 #include <stdio.h>
 
-
-
-
-
-
-
 /**
  *
  *  * main - Entry point
  *
- *   * Description: prints "\"Programming is like building a multilingual puzzle"
+ *   * Description: prints "\"Programming is like building a multilingual puzzl"
  *
  *    * Return: 0
  *
  *     */
 
-
-
-
-
-
-
 int main(void)
-
-
-
 {
 
+	puts("\"Programming is like building a multilingual puzzle");
 
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-
-
-		return (0);
-
-
-
+	return (0);
 }

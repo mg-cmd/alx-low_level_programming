a44f1fae82bfe5fd695713c1115d8eaaebfcdb6d
mg-cmd <gmebrahtu8@gmail.com>
2022-10-13 13:44:26 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,8 @@
 
 #include <string.h>
 
+
+
 /**
  *
  * *main - Main function of my project
@@ -18,13 +20,17 @@ int main(void)
 
 {
 
-          char displayed[52];
 
-	  strcpy(displayed, "\"Programming is like building a multilingual puzz;
 
-			puts(displayed);
+		char displayed[52];
 
-			return (0);
+
+
+			strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+
+				puts(displayed);
+
+					return (0);
+
 }
-
 

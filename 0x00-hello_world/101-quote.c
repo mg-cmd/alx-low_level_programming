b95b53e26689c70a9af386c2b7c8db9930525070
mg-cmd <gmebrahtu8@gmail.com>
2022-10-13 13:01:01 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,26 +1,23 @@
-#include <stdio.h>
-
-#include <unistd.h>
+#include<unistd.h>
 
 /**
  *
- * * main - main function in the task
+ *  * main - prints the standrad error sandwiched around the  standard outpu/t
  *
- * *
+ *   *
  *
- * *
+ *    * Return: returns 1
  *
- * *Return: return 1
- *
- * */
+ *     */
 
 int main(void)
 
 {
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-			return (1);
+	return (1);
 
 }
+
 

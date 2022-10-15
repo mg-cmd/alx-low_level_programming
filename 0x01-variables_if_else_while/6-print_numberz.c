8726c80a4f24b6a880d75cda16fe41af6d18a9fd
Mebrahtu <57825615+mg-cmd@users.noreply.github.com>
2022-10-15 09:25:48 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,39 +1,18 @@
-#include <stdio.h>
-
-
-
-/*
- *
- *   * main - Prints the numbers since 0 to 9
- *
- *     *
- *
- *       * Return: Always (Success)
- *
- */
-
+##include <stdio.h>
+#include <ctype.h>
+/**
+*main - Prints all single digits of base 10 followed by new line
+*
+*Return: returns 0
+*/
 int main(void)
-
 {
+	int digit;
 
-		int n;
-
-
-
-			for (n = '0'; n <= '9'; n++)
-
-					{
-
-								putchar(n);
-
-									}
-
-
-
-				putchar('\n');
-
-
-
-					return (0);
-
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+	}
+	putchar('\n');
+	return (0);
 }

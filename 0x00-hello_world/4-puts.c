80f6e4aa4f7df228a10771be92860a2d284f0f2e
Mebrahtu <57825615+mg-cmd@users.noreply.github.com>
2022-10-15 11:49:03 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
-*main - Main function of my project
-*
-*
-*Return: return 0 everytime
-*/
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
 int main(void)
 {
-
-	char displayed[52];
-
-	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
-	puts(displayed);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
- 
+} 

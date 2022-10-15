d3db89ba235a06ca75a606cea683c1fba4292370
Mebrahtu <57825615+mg-cmd@users.noreply.github.com>
2022-10-15 09:29:49 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,49 +1,27 @@
 #include <stdio.h>
-
-
-
-/*
- *
- *   * main - Prints a hexadecimal string
- *
- *     *
- *
- *       * Return: Always (Success)
- *
- */
-
+#include <ctype.h>
+/**
+*main - Prints all single digits of base 16 followed by new line
+*
+*Return: returns 0
+*/
 int main(void)
-
 {
+	int digit;
 
-		char c;
-
-
-
-			for (c = '0'; c <= '9'; c++)
-
-					{
-
-								putchar(c);
-
-									}
-
-
-
-				for (c = 'a'; c <= 'f'; c++)
-
-						{
-
-									putchar(c);
-
-										}
-
-
-
-					putchar('\n');
-
-
-
-						return (0);
-
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+		if (digit == '9')
+		{
+			digit = 'a';
+			for (; digit <= 'f'; digit++)
+			{
+				putchar(digit);
+			}
+			break;
+		}
+	}
+	putchar('\n');
+	return (0);
 }

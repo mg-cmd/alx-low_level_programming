edbfbc5569c5f87b466611c89af4ddbb96d4e839
Mebrahtu <57825615+mg-cmd@users.noreply.github.com>
2022-10-15 09:35:10 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,65 +1,29 @@
 #include <stdio.h>
-
-
-
 /**
- *
- *   * main - Prints combination of numbers
- *
- *     *
- *
- *       * Return: Always (Success)
- *
- */
-
+*main - Prints all combinations of two digits with,
+* and space followed by new line
+*
+*Return: returns 0
+*/
 int main(void)
-
 {
+	int digit1, digit2;
 
-		int c, i;
+	for (digit1 = 0; digit1 < 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
 
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
 
+			if (digit1 == 8 && digit2 == 9)
+				continue;
 
-			for (c = '0'; c <= '9'; c++)
-
-					{
-
-								for (i = '0'; i <= '9'; i++)
-
-											{
-
-															if (c < i)
-
-																			{
-
-																								putchar(c);
-
-																												putchar(i);
-
-
-
-																																if (c != '8' || (c == '8' && i != '9'))
-
-																																					{
-
-																																											putchar(',');
-
-																																																putchar(' ');
-
-																																																				}
-
-																																			}
-
-																	}
-
-									}
-
-
-
-				putchar('\n');
-
-
-
-					return (0);
-
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }

8230044933fb47e163d27f87d1abae4abd96e5a0
Mebrahtu <57825615+mg-cmd@users.noreply.github.com>
2022-10-18 11:08:05 -0700

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,28 +1,47 @@
 #include "main.h"
 /**
-*times_table - prints the 9 times table
-*
-*Return: returns nothing
-*/
+ * times_table -prints tables
+ *
+ * Return: Always 0.
+ */
 void times_table(void)
 {
-int digit, mult, result;
-for (digit = 0; digit <= 9; digit++)
+int a;
+int b;
+int c;
+for (a = 0; a <= 9; a++)
 {
-_putchar('0');
-
-for (mult = 1; mult <= 9; mult++)
+for (b = 0; b <= 9; b++)
+{
+c = a * b;
+if ((c / 10) == 0)
+{
+if (b == 0)
+{
+_putchar ('0');
+}
+if (b != 0)
+{
+_putchar (' ');
+_putchar ((c % 10) + '0');
+}
+if (b < 9)
 {
 _putchar(',');
-_putchar(' ');
-result = digit * mult;
-if (result <= 9)
-_putchar(' ');
-else
-_putchar((result / 10) + '0');
-
-_putchar((result % 10) + '0');
+_putchar (' ');
 }
-_putchar('\n');
+}
+else
+{
+_putchar ((c / 10) + '0');
+_putchar ((c % 10) + '0');
+if (b < 9)
+{
+_putchar(',');
+_putchar (' ');
+}
+}
+}
+_putchar ('\n');
 }
 }

0fe88996f1ed040e78ed67d262639f646b89832f
Mebrahtu <57825615+mg-cmd@users.noreply.github.com>
2022-10-31 10:38:04 -0700

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,50 +1,27 @@
 #include "main.h"
-
 #include <stdio.h>
 
 /**
-
  * _strpbrk - bytes
-
  * @s: pointer to char
-
  * @accept: pointer to char
-
  * Return: NULL
-
  */
 
-
 char *_strpbrk(char *s, char *accept)
-  
 {
-  
-  int i;
-  
+	int i;
 
-  
-  while (*s)
-    
-    {
-      
-      for (i = 0; accept[i]; i++)
-	
+	while (*s)
 	{
-	  
-	  if (*s == accept[i])
-	    
-	    {
-	      
-	      return (s);
-	      
-	    }
-	  
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
 	}
-      
-      s++;
-      
-    }
-  
-  return (NULL);
-  
+	return (NULL);
 }

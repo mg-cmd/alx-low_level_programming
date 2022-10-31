906c0dadec8d0b1d914f5de631675a7a19e4c77a
Mebrahtu <57825615+mg-cmd@users.noreply.github.com>
2022-10-31 11:46:56 -0700

    Update main.c

--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -1,41 +1,21 @@
 #include "holberton.h"
-
 #include <stdio.h>
 
-
-
 /**
-
  * main - check the code for ALX School students.
-
  *
-
  * Return: Always 0.
-
  */
-
 int main(void)
-  
 {
-  
-  char *s = "hello";
-  
-  char *f;
-  
+    char *s = "hello";
+    char *f;
 
-  
-  f = _strchr(s, 'l');
-  
+    f = _strchr(s, 'l');
 
-  
-  if (f != NULL)
-    
+    if (f != NULL)
     {
-      
-      printf("%s\n", f);
-      
+        printf("%s\n", f);
     }
-  
-  return (0);
-  
+    return (0);
 }

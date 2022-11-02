b4246b977764cc1cdfa127cad8a9c37ce8d17286
Mebrahtu <57825615+mg-cmd@users.noreply.github.com>
2022-11-02 11:49:06 -0700

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,20 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - reverse
- * @s: pointer to string params
- *
+ * _puts_recursion - print a string.
+ * @s: pointer to char
+ * Return: No.
  */
 
-void _print_rev_recursion(char *s)
+void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
-		_print_rev_recursion(s + 1);
 		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+	_putchar('\n');
 	}
 }

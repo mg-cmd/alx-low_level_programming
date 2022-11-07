7634f49c729aba9fdf69dee9dc351926a432fcd9
Mebrahtu <57825615+mg-cmd@users.noreply.github.com>
2022-11-07 10:45:05 -0800

    Update 0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,31 +1,16 @@
 #include "holberton.h"
 
-
-
 /**
+ * _isupper - check the code for Holberton School students.
  *
- *  * _isupper - check the code for Holberton School students.
+ * @c: is an integer param
  *
- *   *
- *
- *    * @c: is an integer param
- *
- *     *
- *
- *      * Return: Always 0.
- *
- *       */
-
-
-
+ * Return: Always 0.
+ */
 
 
 int _isupper(int c)
-
 {
 
-
-
-		return (c >= 65 && c <= 90);
-
+	return (c >= 65 && c <= 90);
 }

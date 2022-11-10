00dcf5a4b91fb69d4303988d5f4b0a037d5b22df
Mebrahtu <57825615+mg-cmd@users.noreply.github.com>
2022-11-10 09:44:34 -0800

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,5 +11,12 @@ int _putchar(char c);
 int _atoi(const char *s);
 void print_int(unsigned long int n);
 
+int _strlen(char *s);
+char *xarray(int size);
+char *_zero_iteration(char *s);
+int convert_to_digit(char s);
+void _product(char *prod, char *mul, int digit, int zeroes);
+void nums_add(char *final_prod, char *next_prod, int next_len);
+
 
 #endif

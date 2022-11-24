4678a7aef4cbf54597ffb26726060c745d4a14ad
Mebrahtu <57825615+mg-cmd@users.noreply.github.com>
2022-11-24 11:09:49 -0800

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,31 +1,25 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
+
 /**
- * print_list -print list.
- *Description: Function that prints content of a single linked list
- * @h: pointer to a structure typedef list_t
- * Return: the number of nodes
- **/
-size_t print_list(const list_t *h)
-{
-/*temp is a list_t type. It is a structure*/
-	const list_t *temp = h;
-	size_t numNodes;
-/*
- * if temp is null, it means it is in the last node. For that reason the
- * condition is: while temp diferent from NULL
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the list_t list
+ *
+ * Return: the number of nodes, and [0] (nil) if str is NULL
  */
 
-	for (numNodes = 0; temp != NULL; numNodes++)
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
 	{
-		if (temp->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
-
 		else
-			printf("[%d] %s\n", temp->len, temp->str);
-
-		temp = temp->next; /*temp will point to the next node*/
+			printf("[%u] %s\n", h->len, h->str);
+		n++;
+		h = h->next;
 	}
-	return (numNodes);
+	return (n);
 }

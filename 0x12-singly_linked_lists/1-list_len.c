63f9608c18008cc71658c024e28ec243c90499c7
Mebrahtu <57825615+mg-cmd@users.noreply.github.com>
2022-11-24 11:10:55 -0800

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /**
- * list_len - number of elements list
- * @h: pointer to structure
- * Return: length.
+ * list_len - returns the numbers of elements in a linked list_t list
+ * @h: pointer to the linked list_t
+ *
+ * Return: the number of elements in a linked list
  */
 
 size_t list_len(const list_t *h)
 {
-
-unsigned int i = 0;
+	size_t n = 0;
 
 	while (h != NULL)
 	{
-		i++;
+		n++;
 		h = h->next;
 	}
-	return (i);
+	return (n);
 }

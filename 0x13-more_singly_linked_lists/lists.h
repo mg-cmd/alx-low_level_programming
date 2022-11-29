5986e851d0b6b880135d4391a1af8dc4270d051e
Mebrahtu <57825615+mg-cmd@users.noreply.github.com>
2022-11-29 09:50:43 -0800

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,10 @@
-#ifndef _LISTS_H
-#define _LISTS_H
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -8,11 +13,18 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
+
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
+
+/*Advanced tasks function prototypes stat*/
+listint_t *reverse_listint(listint_t **head);
+
+/* ends */
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -24,4 +36,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-#endif /* #ifndef _LISTS_H */
+
+#endif /*ifndef LIST_H*/

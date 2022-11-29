7b308a5dd53b43972376600154be37def287b2e5
Mebrahtu <57825615+mg-cmd@users.noreply.github.com>
2022-11-29 10:42:50 -0800

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,56 +1,78 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
+
 /**
- *unique_node_count - counts no. of unique nodes
- *@head:pointer to a head node
- *Return:number of unique nodes,otherwise 0
+ * add_nodeaddress - Add a node to a list with
+ * @head: Pointer to the pointer of the start of the list
+ * @address: The address of another list's node
+ *
+ * Return: Address of new node, NULL if it fails
  */
-size_t unique_node_count(const listint_t *head)
+listadd_t *add_nodeaddress(listadd_t **head, const listint_t *address)
 {
-	listint_t *hare, *tortoise;
-	size_t count = 1;
+	listadd_t *new;
 
-	if (head == NULL || head->next == NULL)
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 	{
-		return (0);
+		free_listadd(*head);
+		exit(98);
 	}
-	hare = head->next->next;
-	tortoise = head->next;
-	while (hare)
-	{
-		if (tortoise == hare)
-		{
-			tortoise = head;
-			while (tortoise != hare)
-			{
-				count++;
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
-			tortoise = tortoise->next;
-			while (tortoise != hare)
-			{
-				count++;
-				tortoise = tortoise->next;
-			}
+	new->address = (void *)address;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
 
 /**
- *print_listint_safe - prints a listint list
- *@head:pointer to head
- *Return:number of nodes in list
+ * free_listadd - Free a list
+ * @head: Pointer to the start of the list
+ */
+void free_listadd(listadd_t *head)
+{
+	listadd_t *killnode;
+
+	while (head != NULL)
+	{
+		killnode = head;
+		head = head->next;
+		free(killnode);
+	}
+}
+
+/**
+ * print_listint_safe - Print out a given list, but only once if it loops
+ * @head: Pointer to the start of the list
+ *
+ * Return: Number of nodes, if it fails print 98
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	unsigned int i = 0;
+	listadd_t *newhead;
+	listadd_t *checker;
+	unsigned int count;
 
-	if (head == NULL)
-		exit(98);
-	while (head)
+	count = 0;
+	newhead = NULL;
+	while (head != NULL)
 	{
-		printf("[%p] %d", (void *)head, head->n);
-		i++;
+		checker = newhead;
+		while (checker != NULL)
+		{
+			if (head == checker->address)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listadd(newhead);
+				return (count);
+			}
+			checker = checker->next;
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		add_nodeaddress(&newhead, head);
 		head = head->next;
+		count++;
 	}
-	return (i);
+	free_listadd(newhead);
+	return (count);
 }

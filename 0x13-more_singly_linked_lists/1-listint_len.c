df34cb915e392cb74476d93ca40d5e0e58bf44bc
Mebrahtu <57825615+mg-cmd@users.noreply.github.com>
2022-11-29 16:42:45 +0300

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,41 +1,21 @@
 #include "lists.h"
 
-
-
 /**
- *
- *  * listint_len - Calculate the number of elements.
- *
- *   * @h: Pointer to a list.
- *
- *    * Return: Integer.
- *
- *     **/
-
-
+ * listint_len - Calculate the number of elements.
+ * @h: Pointer to a list.
+ * Return: Integer.
+ **/
 
 size_t listint_len(const listint_t *h)
-
 {
+	const listint_t *tp;
+	unsigned int cnr = 0;
 
-		const listint_t *tp;
-
-			unsigned int cnr = 0;
-
-
-
-				tp = h;
-
-					while (tp)
-
-							{
-
-										cnr++;
-
-												tp = tp->next;
-
-													}
-
-						return (cnr);
-
+	tp = h;
+	while (tp)
+	{
+		cnr++;
+		tp = tp->next;
+	}
+	return (cnr);
 }

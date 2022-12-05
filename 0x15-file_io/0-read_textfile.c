f7f91634318aafe842a7a9149a442d50c4c5a1a9
Mebrahtu <57825615+mg-cmd@users.noreply.github.com>
2022-12-05 16:48:50 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,41 +1,33 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
- *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
+ *read_textfile - reads a text file and prints it to the
+ *POSIX standard output
+ *@filename:pointer to file description
+ *@letters:number of letters it should read and print
+ *Return:actual no. of letters it could read and print
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	 ssize_t o, r, w;
-	char *buffer;
+	int fd, bytes;
+	char *buf;
 
+/*check if pointer is NULL or letters < 1*/
 	if (filename == NULL)
 		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer ==  NULL)
+	if (letters < 1)
 		return (0);
 
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buffer);
+	fd = open(filename, O_RDONLY);
+/*if the process is not successful*/
+	if (fd == -1)
 		return (0);
-	}
+	buf = malloc(sizeof(char) * letters);
+	bytes = read(fd, buf, letters);
+	buf[bytes + 1] = '\0';
 
-	free(buffer);
-	close(o);
-
-	return (w);
-
+	write(STDOUT_FILENO, buf, bytes);
+	close(fd);
+	free(buf);
+	return (bytes);
 }
